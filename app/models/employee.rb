e251e561b7519a6797d2e02b7e698765d9928067
Jordi Puyoles <jordi.puyoles.calafell@gmail.com>
2025-05-04 17:22:10 +0200

    Mail Validation & Employee Required Fields [name, email] Added

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,3 +1,6 @@
 class Employee < ApplicationRecord
     has_many :time_off_requests, dependent: :destroy
+
+    validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+    validates :name, presence: true
 end
